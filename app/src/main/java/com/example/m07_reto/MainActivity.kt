6f76e622c9dd986e84e64bfc778d9cac48499333
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-01-16 10:14:05 +0100

    cosas

--- app/src/main/java/com/example/m07_reto/MainActivity.kt
+++ app/src/main/java/com/example/m07_reto/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.m07_reto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val handler = Handler()
+
+// Establece el progreso inicial en 0
+        progressBar.progress = 0
+
+// Ejecuta el código para actualizar el progreso después de 10 segundos
+        handler.postDelayed({
+            progressBar.progress = 100
+            progressBar.progress = 100
+        }, 10000)
+
+        val intent = Intent(this, pantalla_listado::class.java)
+        startActivity(intent)
+
+
     }
 }
