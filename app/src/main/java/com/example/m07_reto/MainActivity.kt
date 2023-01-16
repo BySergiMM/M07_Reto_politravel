c9fa82d9039ce7caf0301911f1affc333d87a242
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-01-16 20:11:16 +0100

    primera pantalla acabada

--- app/src/main/java/com/example/m07_reto/MainActivity.kt
+++ app/src/main/java/com/example/m07_reto/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.m07_reto
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
+private lateinit var progressBar: ProgressBar
+private val delay: Long = 10000 // 10 segundos
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        progressBar = findViewById(R.id.progressbar)
+        progressBar.progress = 0
+
+        val task = object : CountDownTimer(delay, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                progressBar.progress = 100 - (millisUntilFinished / 100).toInt()
+            }
+            override fun onFinish() {
+                finish()
+                val intent = Intent(this@MainActivity, lista::class.java)
+                startActivity(intent)
+            }
+        }.start()
 
         val pantalla = findViewById<View>(R.id.jueves)
 
         pantalla.setOnClickListener {
+            finish()
             val intent = Intent(this, lista::class.java)
             startActivity(intent)
         }
