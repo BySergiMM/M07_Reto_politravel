5aac89b3a1d4edc36e5f060d4e7596c49ce6efbd
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-01-16 18:51:27 +0100

    a

--- app/src/main/java/com/example/m07_reto/MainActivity.kt
+++ app/src/main/java/com/example/m07_reto/MainActivity.kt
@@ -1,37 +1,20 @@
 package com.example.m07_reto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.Handler
 import android.view.View
-import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var progressBar: ProgressBar
-    private var countDownTimer: CountDownTimer? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        progressBar = findViewById(R.id.progressBar)
-        progressBar.max = 10
+        
 
-        countDownTimer = object : CountDownTimer(10000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                progressBar.progress = (10000 - millisUntilFinished).toInt() / 1000
-            }
+        val pantalla = findViewById<View>(R.id.jueves)
 
-            override fun onFinish() {
-
-            }
-        }.start()
-
-        findViewById<View>(R.id.jueves).setOnClickListener {
-            countDownTimer?.cancel()
+        pantalla.setOnClickListener {
             val intent = Intent(this, lista::class.java)
             startActivity(intent)
         }
