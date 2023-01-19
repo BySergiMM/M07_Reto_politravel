f74e5e39b528076fe8291f15a33f6401755fbace
smartim2122 <98814441+BySergiMM@users.noreply.github.com>
2023-01-19 09:24:46 +0100

    muchas cosas

--- app/src/main/java/com/example/m07_reto/DestinosAdapter.kt
+++ app/src/main/java/com/example/m07_reto/DestinosAdapter.kt
@@ -1,18 +1,33 @@
 package com.example.m07_reto
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class DestinosAdapter (private val context: Context, private val destinos: List<Destinos>)
     : RecyclerView.Adapter<DestinosAdapter.DestinosViewHolder>() {
 
+    private val layout = R.layout.item_lista
+
+    class DestinosViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        var nombre : TextView
+        var imagen : ImageView
+        var transporte : TextView
+        init {
+            nombre = view.findViewById(R.id.nombre)
+            imagen = view.findViewById(R.id.imagen)
+            transporte = view.findViewById(R.id.transporte)
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DestinosViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_lista, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return DestinosViewHolder(view)
     }
 
@@ -22,18 +37,15 @@ class DestinosAdapter (private val context: Context, private val destinos: List<
 
     override fun onBindViewHolder(holder: DestinosViewHolder, position: Int) {
         val destino = destinos[position]
-        holder.bind(destino)
+        bind(holder, destino)
     }
 
-    class DestinosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val nombreTextView: TextView = itemView.findViewById(R.id.nombre)
-        private val transporteTextView: TextView = itemView.findViewById(R.id.transporte)
-        private val imagenImageView: ImageView = itemView.findViewById(R.id.imagen)
+    fun bind(holder: DestinosViewHolder, destinos: Destinos) {
+        holder.nombre?.text = destinos.nombre
+        holder.transporte?.text = destinos.transporte
 
-        fun bind(destino: Destinos) {
-            nombreTextView.text = destino.nombre
-            transporteTextView.text = destino.transporte
-            imagenImageView.setImageResource(destino.imagen)
-        }
+        val paqueteruta = context.getFilesDir().toString() +"/img/"+ destinos.imagen
+        val bitmap = BitmapFactory.decodeFile(paqueteruta)
+        holder.imagen?.setImageBitmap(bitmap)
     }
 }
