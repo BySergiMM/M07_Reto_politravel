0d65f329a0933c62017d9b7ebac1e1bbaf9bbae4
smartim2122 <98814441+BySergiMM@users.noreply.github.com>
2023-01-27 13:25:17 +0100

    BUNITO

--- app/src/main/java/com/example/m07_reto/PantallaDetalle.kt
+++ app/src/main/java/com/example/m07_reto/PantallaDetalle.kt
@@ -17,70 +17,79 @@ class PantallaDetalle : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
 
-        val intent = getIntent()
-        val objeto = intent.getSerializableExtra("objeto") as ClaseDestinos
+//----------------------------------      VARIABLES       ------------------------------------//
 
-        var Titulo = findViewById<TextView>(R.id.TituloVer)
-        val Imagen = findViewById<ImageView>(R.id.ImagenVer)
-        val Dias = findViewById<TextView>(R.id.DiasVer)
-        val Transporte = findViewById<TextView>(R.id.TransporteVer)
-        val Inicio = findViewById<TextView>(R.id.InicioVer)
-        val Final = findViewById<TextView>(R.id.FinVer)
-        val FtoTransporte = findViewById<ImageView>(R.id.transporte)
+        val intent      = getIntent()
+        val objeto      = intent.getSerializableExtra("objeto") as ClaseDestinos
 
-        val latitud = findViewById<TextView>(R.id.latitud)
-        val longitud = findViewById<TextView>(R.id.longitud)
+        val Titulo      = findViewById<TextView>(R.id.TituloVer)
+        val Imagen      = findViewById<ImageView>(R.id.ImagenVer)
+        val Dias        = findViewById<TextView>(R.id.DiasVer)
+        val Transporte  = findViewById<TextView>(R.id.TransporteVer)
+        val Inicio      = findViewById<TextView>(R.id.InicioVer)
+        val Final       = findViewById<TextView>(R.id.FinVer)
+        val FtoTrans    = findViewById<ImageView>(R.id.transporte)
 
+        val latitud     = findViewById<TextView>(R.id.latitud)
+        val longitud    = findViewById<TextView>(R.id.longitud)
 
-        val ruta = getFilesDir().toString() +"/img/"+ objeto.imagen
-        val bitmap = BitmapFactory.decodeFile(ruta)
+        val ruta        = getFilesDir().toString() +"/img/"+ objeto.imagen
+        val bitmap      = BitmapFactory.decodeFile(ruta)
+
+//--------------------------------       INICIALICAMOS       --------------------------------//
 
         Imagen?.setImageBitmap(bitmap)
 
-        Titulo.text = objeto.nombre
-        Dias.text = objeto.dias.toString()
+        Titulo.text     = objeto.nombre
+        Dias.text       = objeto.dias.toString()
         Transporte.text = objeto.transporte
-        Inicio.text = objeto.inicio
-        Final.text = objeto.fin
+        Inicio.text     = objeto.inicio
+        Final.text      = objeto.fin
 
-        latitud.text = objeto.Coordenadas.latitud
-        longitud.text = objeto.Coordenadas.longitud
-
-        var latitudMaps = objeto.Coordenadas.latitud.toDouble()
-        var longitudMaps = objeto.Coordenadas.longitud.toDouble()
+        latitud.text    = objeto.Coordenadas.latitud
+        longitud.text   = objeto.Coordenadas.longitud
 
         if (Transporte.text == "Avion")
         {
-            FtoTransporte.setImageResource(R.drawable.avion)
+            FtoTrans.setImageResource(R.drawable.avion)
         } else if (Transporte.text == "Barco")
         {
-            FtoTransporte.setImageResource(R.drawable.barco)
+            FtoTrans.setImageResource(R.drawable.barco)
         } else if (Transporte.text == "Coche")
         {
-            FtoTransporte.setImageResource(R.drawable.coche)
+            FtoTrans.setImageResource(R.drawable.coche)
         }
+
+//-------------------------------------      MAPS       ---------------------------------------//
+
+        var latitudMaps = objeto.Coordenadas.latitud.toDouble()
+        var longitudMaps = objeto.Coordenadas.longitud.toDouble()
 
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync { googleMap ->
+
             //Posición inicial del mapa
             val latLng = LatLng(latitudMaps, longitudMaps)
             val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10f)
             googleMap.moveCamera(cameraUpdate)
+
             //Agregando marcador en la posicion
             googleMap.addMarker(MarkerOptions().position(latLng))
+
             //Habilitando los controles de zoom
             googleMap.uiSettings.isZoomControlsEnabled = true
+
         }
 
+//-------------------------------------    ITINERARIO    --------------------------------------//
 
-        val listaItinerario = findViewById<RecyclerView>(R.id.listaItinerario)
-
-        val AdapterIti = ItinerariAdapter(this, objeto.itinerario)
+        val listaItinerario     = findViewById<RecyclerView>(R.id.listaItinerario)
+        val AdapterIti          = ItinerariAdapter(this, objeto.itinerario)
 
         listaItinerario.layoutManager = LinearLayoutManager(this)
-        listaItinerario.adapter = AdapterIti
+        listaItinerario.adapter       = AdapterIti
 
         listaItinerario.isNestedScrollingEnabled = false
 
