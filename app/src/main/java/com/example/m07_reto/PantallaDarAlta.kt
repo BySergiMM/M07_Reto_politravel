1d06bf4c0682d8353dcb5d10a0094ebcfe7e854b
smartim2122 <98814441+BySergiMM@users.noreply.github.com>
2023-02-01 13:52:53 +0100

    spinner

--- app/src/main/java/com/example/m07_reto/PantallaDarAlta.kt
+++ app/src/main/java/com/example/m07_reto/PantallaDarAlta.kt
@@ -2,10 +2,39 @@ package com.example.m07_reto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 
 class PantallaDarAlta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_dar_alta)
+
+        val spinner: Spinner = findViewById(R.id.aTransportePaquete)
+        val listaSpinner = listOf("Coche", "Avion", "Tren", "Barco")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaSpinner)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ){
+                val selectedItem = parent?.getItemAtPosition(position) as String
+                val textView = spinner.getChildAt(0) as TextView
+                textView.setTextSize(20f)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
     }
 }
