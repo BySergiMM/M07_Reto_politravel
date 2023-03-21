b38f6936e2bcf65b9370c61c3f68612a0725b17d
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-03-21 13:17:09 +0100

    a

--- app/src/main/java/com/example/sergimartio_politravel/ClaseItinerari.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sergimartio_politravel
-
-import java.io.Serializable
-
-class ClaseItinerari (val dia: Int, val actividad: String): Serializable
--- app/src/main/java/com/example/sergimartio_politravel/Coordenadas.kt
+++
@@ -1,4 +0,0 @@
-package com.example.sergimartio_politravel
-import java.io.Serializable
-
-class Coordenadas (val latitud: String, val longitud: String): Serializable
