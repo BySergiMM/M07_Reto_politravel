f5d29a424e4bf9645ec9740773692907cba164b7
BySergiMM <98814441+BySergiMM@users.noreply.github.com>
2023-03-21 10:02:37 +0100

    a

--- app/src/main/java/com/example/sergimartio_politravel/MainActivity.kt
+++ app/src/main/java/com/example/sergimartio_politravel/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.sergimartio_politravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private fun startImageTimer() {
+        val handler = Handler()
+        val imageTimer = Timer()
+
+        imageTimer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    val randomIndex = (0 until imageList.size).random()
+                    myImageView.setImageResource(imageList[randomIndex])
+                }
+            }
+        }, 0, 3000)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val myImageView = findViewById<ImageView>(R.id.myImageView)
+        val imageList = listOf(R.drawable.desierto, R.drawable.playa, R.drawable.montanas)
+
+        startImageTimer()
     }
 }
